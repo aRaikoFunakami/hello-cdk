b1e2908014c0bba487ab0b1d032bac4c77587edf
aRaiko.Funakami <raiko.funakami@access-company.com>
2025-02-16 15:03:15 +0900

    [add] add amazon api gateway

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,16 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// Import the Lambda module
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+// Import API Gateway L2 construct
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define the Lambda function resource
+    const helloWorldFunction = new lambda.Function(this, 'HelloWorldFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X, // Choose any supported Node.js runtime
+      code: lambda.Code.fromAsset('lambda'), // Points to the lambda directory
+      handler: 'hello.handler', // Points to the 'hello' file in the lambda directory
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    cdk.Tags.of(helloWorldFunction).add('WPS', 'cdktest_rairaii');
+
+    // Define the API Gateway resource
+    const api = new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
+      handler: helloWorldFunction,
+      proxy: false,
+    });
+
+    cdk.Tags.of(api).add('WPS', 'cdktest_rairaii');   
+        
+    // Define the '/hello' resource with a GET method
+    const helloResource = api.root.addResource('hello');
+    helloResource.addMethod('GET');
   }
 }
